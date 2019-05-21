4935691be0e2d3147b7a0521de60243d7e119bc5
adriano_henrique <adriano_henrique@bitbucket.org>
2019-05-20 22:28:59 -0300

    Método iterativo para inversao de Strings

--- src/com/chapter/recursao/reversearray/ReverseArray.java
+++ src/com/chapter/recursao/reversearray/ReverseArray.java
@@ -3,7 +3,8 @@ package com.chapter.recursao.reversearray;
 import java.util.Arrays;
 
 public class ReverseArray {
-
+	//recursao final so existe quando um metodo recursivo chama ele mesmo como a ultima coisa que ele faz no metodo
+	//o metodo abaixo é recursivo, mas não usa recursao final
 	public static String[] reverseArray(String[] array, int i, int j) {
 		if (i == j) return array;
 		if (i < j) {
@@ -14,7 +15,20 @@ public class ReverseArray {
 		return reverseArray(array, i + 1, j - 1);
 	}
 	
+	//metodo iterativo de inversao de strings
+	public static String[] reverseArrayIterative(String[] array, int i, int j) {
+		while (i < j) {
+			String aux = array[j];
+			array[j] = array[i];
+			array[i] = aux;
+			i++;
+			j--;
+		}
+		return array;
+	}
+	
 	public static void main(String[] args) {
 		System.out.println(Arrays.toString(reverseArray(new String[]{"A", "d", "r", "i", "a", "n", "o"}, 0, 6)));
+		System.out.println(Arrays.toString(reverseArrayIterative(new String[]{"A", "d", "r", "i", "a", "n", "o"}, 0, 6)));
 	}
 }
